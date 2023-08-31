df4405b020fba3a5bc276d2565889acdcad679bf
IlyaChichkov <ilya555chich@yandex.ru>
2023-08-31 11:31:27 +0300

    repo: Initial commit

---
+++ s_axi_reg.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/31/2023 11:15:56 AM
+// Design Name: 
+// Module Name: s_axi_reg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module s_axi_reg(
+
+    );
+endmodule
