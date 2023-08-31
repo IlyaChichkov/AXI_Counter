075830612d7b961ade50b876dfe859c156ef3ca8
IlyaChichkov <ilya555chich@yandex.ru>
2023-08-31 12:03:00 +0300

    feat: добавил входные и выходные сигналы модуля

--- s_axi_reg.sv
+++ s_axi_reg.sv
@@ -21,6 +21,39 @@
 
 
 module s_axi_reg(
-
+    // GLOBAL SIGNALS
+    input               clk,        
+    input               areset,
+    // WRITE SIGNALS
+    //   Address
+    input   [3:0]       awid_i,
+    input   [31:0]      awaddr_i,
+    input               awvalid_i,
+    output              awready_o
+    //   Data
+    input   [3:0]       wid_i,
+    input   [31:0]      wdata_i,
+    input   [3:0]       wstrb_i,
+    input               wlast_i,
+    input               wvalid_i,
+    input               wready_i,
+    // READ SIGNALS
+    //   Address
+    input   [3:0]       arid_i,
+    input   [31:0]      araddr_i,
+    input               arvalid_i,
+    output              arready_o
+    //   Data
+    input   [3:0]       rid_i,
+    input   [31:0]      rdata_i,
+    input   [3:0]       rstrb_i,
+    input               rlast_i,
+    input               rvalid_i,
+    input               rready_i,
+    // RESPONSE SIGNALS
+    output  [3:0]       bid_o,
+    output  [1:0]       bresp_o,
+    output              bvalid_o,
+    input               bready_i,
     );
 endmodule
