36a27143436563887ce8fa9d2469a3cce9e768e9
IlyaChichkov <ilya555chich@yandex.ru>
2023-08-31 13:41:46 +0300

    repo: добавил пустой testbench

---
+++ testbench.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/31/2023 01:18:45 PM
+// Design Name: 
+// Module Name: testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench(
+
+    );
+endmodule
