64ef1f0520ef0888f95643497ce21879a7f4ade6
IlyaChichkov <ilya555chich@yandex.ru>
2023-09-04 16:16:38 +0300

    feat: разбил этапы AXI на независимые блоки

--- testbench.sv
+++ testbench.sv
@@ -37,9 +37,22 @@ end
 
 initial begin
     reset();
-    s_write_data();
-    reset();
-    s_write_data_before_addr();
+    write_data(32'hC2CCEE2E);
+    uncheck_vdata();
+    write_data(32'hA3DDDD3F);
+    uncheck_vdata();
+    write_addr(32'd1);
+    wait_waddr_ready();
+    wait_response_ready();
+    write_addr(32'd2);
+    wait_waddr_ready();
+    write_addr(32'd3);
+    write_data(32'hA477772D);
+    uncheck_vdata();
+    wait_response_ready();
+    write_data(32'hF19F4125);
+    uncheck_vdata();
+    write_addr(32'd4);
 
     m_read_data(1);
 end
@@ -102,9 +115,91 @@ task reset();
     araddr_i = 0;
     arvalid_i = 0;
     rready_i = 0;
+    #40;
+    areset = 1;
     #20;
+
 endtask
 
+
+task write_data(
+    input logic [31:0]  data
+    );
+    wstrb_i = 4'b1111;
+    wdata_i = data;
+    #7;
+    wvalid_i = '1;
+endtask
+
+task write_addr(
+    input logic [31:0] addr
+    );
+    awaddr_i = addr;
+    #7;
+    awvalid_i = '1;
+endtask
+
+task uncheck_vaddr();
+    #10;
+    awvalid_i = '0;
+endtask
+
+task uncheck_vdata();
+    #10;
+    wvalid_i = '0;
+endtask
+
+task wait_waddr_ready();
+    do begin
+        @(posedge clk);
+    end while(!awready_o); 
+    awvalid_i = '0;
+endtask
+
+task wait_wdata_ready();
+    do begin
+        @(posedge clk);
+    end while(!wready_o); 
+    wvalid_i = '0;
+endtask
+
+task wait_response_ready();
+    do begin
+        @(posedge clk);
+    end while(!bvalid_o); 
+    bready_i = '1;
+    #10;
+    bready_i = '0;
+endtask
+
+task m_read_data(input int addr);
+    areset = 1;
+    #20;
+    araddr_i = addr;
+    rready_i = '0;
+    #7
+    arvalid_i = '1;
+    
+    do begin
+        @(posedge clk);
+    end while(arready_o); 
+    arvalid_i = '0;
+    
+    #10;
+
+    do begin
+        @(posedge clk);
+    end while(!rvalid_o); 
+    
+    #20;
+    rready_i = '1;
+    #20;
+    rready_i = '0;
+endtask
+
+endmodule
+
+/*
 task s_write_data_before_addr();
     #20;
     areset = 1;
@@ -174,40 +269,4 @@ task s_write_data();
     #20;
     bready_i = '0;
 endtask
-
-
-/*
-
-logic [3:0]         arid_i;
-logic [31:0]        araddr_i;
-logic               arvalid_i;
-
-logic               rready_i;
 */
-
-task m_read_data(input int addr);
-    areset = 1;
-    #20;
-    araddr_i = addr;
-    rready_i = '0;
-    #7
-    arvalid_i = '1;
-    
-    do begin
-        @(posedge clk);
-    end while(arready_o); 
-    arvalid_i = '0;
-    
-    #10;
-
-    do begin
-        @(posedge clk);
-    end while(!rvalid_o); 
-    
-    #20;
-    rready_i = '1;
-    #20;
-    rready_i = '0;
-endtask
-
-endmodule
