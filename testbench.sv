8ca2094e884491ca0da0f80447ccd50cfd4be27b
IlyaChichkov <ilya555chich@yandex.ru>
2023-09-04 10:47:52 +0300

    feat (testbench): тест записи данных с отправкой адреса после данных

--- testbench.sv
+++ testbench.sv
@@ -36,7 +36,10 @@ forever
 end
 
 initial begin
+    reset();
     s_write_data();
+    reset();
+    s_write_data_before_addr();
 end
 
 logic   [3:0]      awid_i;
@@ -70,48 +73,86 @@ s_axi_reg slave(
     .bready_i       (bready_i)
 );
 
-task s_write_data();
+task reset();
+    #20;
     awid_i = 0;
     awaddr_i = 0;
     wdata_i = 0;
     bready_i = 0;
     awvalid_i = 0;
+    wstrb_i = 0;
     wvalid_i = 0;
+    #20;
+endtask
 
-    for (int i = 0; i < 10; i++) begin
-        $display("\tWrite data i=%0d",i);
-        #20;
-        areset = 1;
-        #20;
-        awaddr_i = 32'b1;
+task s_write_data_before_addr();
+    #20;
+    areset = 1;
+    #20;
 
-        #7
-        awvalid_i = '1;
-        
-        do begin
-            @(posedge clk);
-        end while(!awready_o); 
-        awvalid_i = '0;
+    wstrb_i = 4'b1010;
+    wdata_i = 32'hEFDBCA54;
+    #20;
+    wvalid_i = '1;
 
-        wstrb_i = 4'b1010;
-        wdata_i = 32'hABCDEFAC;
-        #10;
-        wvalid_i = '1;
+    awaddr_i = 32'b1;
 
-        do begin
-            @(posedge clk);
-        end while(!wready_o); 
-        wvalid_i = '0;
+    #7
+    awvalid_i = '1;
+    
+    do begin
+        @(posedge clk);
+    end while(!awready_o); 
+    awvalid_i = '0;
 
 
-        do begin
-            @(posedge clk);
-        end while(!bvalid_o); 
-        #5;
-        bready_i = '1;
-        #20;
-        bready_i = '0;
-    end
+    do begin
+        @(posedge clk);
+    end while(!wready_o); 
+    wvalid_i = '0;
+
+
+    do begin
+        @(posedge clk);
+    end while(!bvalid_o); 
+    #5;
+    bready_i = '1;
+    #20;
+    bready_i = '0;
+endtask
+
+task s_write_data();
+    #20;
+    areset = 1;
+    #20;
+    awaddr_i = 32'b1;
+
+    #7
+    awvalid_i = '1;
+    
+    do begin
+        @(posedge clk);
+    end while(!awready_o); 
+    awvalid_i = '0;
+
+    wstrb_i = 4'b1010;
+    wdata_i = 32'hABCDEFAC;
+    #10;
+    wvalid_i = '1;
+
+    do begin
+        @(posedge clk);
+    end while(!wready_o); 
+    wvalid_i = '0;
+
+
+    do begin
+        @(posedge clk);
+    end while(!bvalid_o); 
+    #5;
+    bready_i = '1;
+    #20;
+    bready_i = '0;
 endtask
 
 endmodule
